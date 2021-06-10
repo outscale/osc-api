0628d5b6baec91b59394a655b5e295ac452472d6
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-06-10 11:02:36 +0200

    Trigger osc-sdk-go auto-build on release
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ .github/scripts/build-osc-sdk-go.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ -z "$API_VERSION" ]; then
+    echo "API_VERSION is missing, abort"
+    echo "pass API_VERSION through environment variable"
+    exit 1
+fi
+
+if [ -z "$GH_TOKEN" ]; then
+    echo "GH_TOKEN is missing, abort."
+    echo "pass GH_TOKEN through environment variable"
+    exit 1
+fi
+
+ghurl="https://api.github.com"
+data="{\"ref\": \"v2\", \"inputs\": {\"api_version\": \"$API_VERSION\"}"
+curl \
+    -X POST \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -d "${data}" \
+    "${ghurl}/repos/outscale/osc-sdk-go/actions/workflows/build.yml/dispatches"
